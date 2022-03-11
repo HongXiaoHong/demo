7564f28f335b7644bb12fcc525134dc101395593
HongXiaoHong <1908711045@qq.com>
2022-03-11 23:35:31 +0800

    es6 npm 体验 mocha babel 转换 es6 to es5

---
+++ example.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let app = '抖音'
+console.log("app")
+
+// if (true) let a = 1
+// SyntaxError: Lexical declaration cannot appear in a single-statement context
+
+if (true) {
+    let a = 1
+}
