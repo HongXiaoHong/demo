15d6d2c781e82f77686750946e86a170df007509
洪晓鸿 <1908711045@qq.com>
2023-07-29 09:12:35 +0800

    向普通用户展示书籍信息 - 直接继承 django 内置的界面逻辑 - 通过覆盖对应的页面, 向用户展示不同的界面

--- hello_django/locallibrary/locallibrary/catalog/views.py
+++ hello_django/locallibrary/locallibrary/catalog/views.py
@@ -1,7 +1,9 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 
 def index(request):
@@ -21,4 +23,28 @@ def index(request):
         'index.html',
         context={'num_books': num_books, 'num_instances': num_instances,
                  'num_instances_available': num_instances_available, 'num_authors': num_authors},
+    )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+def book_detail_view(request, pk):
+    try:
+        book_id = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        raise Http404("Book does not exist")
+
+    # book_id=get_object_or_404(Book, pk=pk)
+
+    return render(
+        request,
+        'catalog/book_detail.html',
+        context={'book': book_id, }
     )
