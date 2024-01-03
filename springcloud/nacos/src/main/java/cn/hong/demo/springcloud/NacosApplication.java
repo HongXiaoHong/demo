a9f2e4cfd953e7ff6bebb40057b580ab7a4769bf
洪晓鸿 <1908711045@qq.com>
2024-01-04 00:24:24 +0800

    搭建 springcloud nacos 微服务

---
+++ springcloud/nacos/src/main/java/cn/hong/demo/springcloud/NacosApplication.java
@@ -0,0 +1,16 @@
+package cn.hong.demo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * nacos 启动测试类
+ */
+@SpringBootApplication
+
+public class NacosApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NacosApplication.class, args);
+  }
+}
